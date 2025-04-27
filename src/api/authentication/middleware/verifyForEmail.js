fc6f85f30bdf12108c92f7248def3f095adc1b35
yassine <yassineova@gmail.com>
2025-04-28 00:14:29 +0100

    ee

--- src/api/authentication/middleware/verifyForEmail.js
+++ src/api/authentication/middleware/verifyForEmail.js
@@ -1,5 +1,5 @@
 const User = require('../../../models/User');
-const generateTokens = require('../../../middlewares/generateTokens');
+const generateTokens = require('../../../middleware/generateTokens');
 
 exports.verifyForEmail = async (req, res) => {
     const { code } = req.body;
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,13 @@
 const app = require("express")();
 
+const authRoutes = require("./authentication/routes");
+const applicationRoutes = require("./application/routes");
+const messageRoutes = require("./message/routes");
+const userRoutes = require("./user/routes");
 
+app.use("/auth", authRoutes);
+app.use("/application", applicationRoutes);
+app.use("/message", messageRoutes);
+app.use("/user", userRoutes);
 
 module.exports = app;
